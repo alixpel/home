6021d60bd1df361366fbbf8b0cb3d044abd29d25
Alix Pelletier <alixpelletierpro@gmail.com>
2025-06-29 18:45:59 +0200

    Update theme.js h1 theme exclusion

--- theme.js
+++ theme.js
@@ -1,6 +1,6 @@
 // nuit
 document.querySelector('.night-button').addEventListener('click', function(){
-	document.querySelector("h1").style.color = '#F4FFFD';
+	//document.querySelector("h1").style.color = '#F4FFFD';
 	document.querySelector("p").style.color = '#F4FFFD';
 	document.querySelector(".day-button").style.color = '#465362';
 	document.querySelector(".night-button").style.display = "none";
@@ -13,7 +13,7 @@ document.querySelector('.night-button').addEventListener('click', function(){
 
 // jour
 document.querySelector('.day-button').addEventListener('click', function(){
-	document.querySelector("h1").style.color = '#011936';
+	//document.querySelector("h1").style.color = '#011936';
 	document.querySelector("p").style.color = '#011936';
 	document.body.style.backgroundColor = '#F4FFFD';
 	document.querySelector(".night-button").style.color = '#f3f3f3';
@@ -25,7 +25,7 @@ document.querySelector('.day-button').addEventListener('click', function(){
 });
 document.querySelector('.jacquouillenuit').style.display = "none";
 document.body.style.backgroundColor = '#F4FFFD';
-document.querySelector("h1").style.color = '#011936';
+//document.querySelector("h1").style.color = '#011936';
 document.querySelector("p").style.color = '#011936';
 document.querySelector(".day-button").style.display = "none";
 
